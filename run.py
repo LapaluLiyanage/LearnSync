6d14d70b38df89630e948a6dd9cca89b7c57df9a
Lapalu Liyanage <lapaluliyanage@gmail.com>
2025-03-21 16:49:06 +0530

    #

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'app')))
 
 from app import create_app, db
-from app.models.user import User
-from app.models.group import Group, GroupMember
-from app.models.material import Material, AISummary
-from app.models.chat import Message
-from app.models.schedule import StudySession, SessionParticipant
+from learnsync.app.models.user import User
+from learnsync.app.models.group import Group, GroupMember
+from learnsync.app.models.material import Material, AISummary
+from learnsync.app.models.chat import Message
+from learnsync.app.models.schedule import StudySession, SessionParticipant
 
 app = create_app()
 
